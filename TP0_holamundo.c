6f9d8f59701a1b7cfe09a42a4ed7c2a863fb9046
FedericoMandri <fede_mandri@yahoo.com.ar>
2015-04-07 00:41:17 -0300

    actualiza tp0

--- TP0_holamundo.c
+++ TP0_holamundo.c
@@ -1,11 +1,12 @@
 /* Imprime en pantalla el string "Hola Mundo"
 * Federico Mandri 
-* 03-04-2015
+* 07-04-2015
 */
 
 #include <stdio.h>
 
 int main (void)
 {
-     printf ("Hola Mundo");
+     printf ("Hola Mundo\n");
+     getchar();
 }
